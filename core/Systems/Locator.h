e170e11f924a0c1fd489307513d1d6a8508ffb3c
neo-bolea <neo@bolea.org>
2019-10-26 11:10:25 +0200

    Put definition of CmdNode outside Locator

--- core/Systems/Locator.h
+++ core/Systems/Locator.h
@@ -26,37 +26,7 @@ class Locator : public Singleton<Locator>
 public:
 	// Helper class to construct a tree of functions that fit certain key sequences,
 	// passed to the Locator.
-	struct CmdNode
-	{
-	public:
-		// Nest another object inside this one
-		CmdNode(CmdKey cmd, const CmdNode &inner);
-
-		// Nest multiple objects inside this one
-		template<typename ...Args>
-		CmdNode(CmdKey cmd, const Args &...inners);
-
-		// Link a free function to this object
-		template<typename ...FuncArgs>
-		CmdNode(CmdKey cmd, void(*cb)(FuncArgs...));
-
-		// Link a member function to this object
-		template<typename Member, typename ...FuncArgs>
-		CmdNode(CmdKey cmd, Member *toBind, void(Member::*cb)(FuncArgs...));
-
-		// Either contains nested objects or a callable
-		LeafFunc leafFunc;
-		std::vector<CmdNode> nodes;
-
-		// The key that fits the current tree branch, to create the key sequence.
-		CmdKey levelKey;
-
-	private:
-		// Enables the use of a vector for functions with different signatures,
-		// by bottlenecking to void *.
-		template<typename Signature, typename ...FuncArgs>
-		static void variableFunctionSignature(void *args, const type_info &info, Signature func);
-	};
+	struct CmdNode;
 
 	// Add functions to given key sequences.
 	void Add(CmdNode &tree);
@@ -78,6 +48,40 @@ private:
 	std::vector<keySequenceInfo> traverseTree(CmdNode &node);
 
 	std::vector<LeafFunc> cmds[keySequences::largestPossibleBit()];
+};
+
+
+struct Locator::CmdNode
+{
+public:
+	// Nest another object inside this one
+	CmdNode(CmdKey cmd, const CmdNode &inner);
+
+	// Nest multiple objects inside this one
+	template<typename ...Args>
+	CmdNode(CmdKey cmd, const Args &...inners);
+
+	// Link a free function to this object
+	template<typename ...FuncArgs>
+	CmdNode(CmdKey cmd, void(*cb)(FuncArgs...));
+
+	// Link a member function to this object
+	template<typename Member, typename ...FuncArgs>
+	CmdNode(CmdKey cmd, Member *toBind, void(Member::*cb)(FuncArgs...));
+
+
+	// Either contains nested objects or a callable
+	LeafFunc leafFunc;
+	std::vector<CmdNode> nodes;
+
+	// The key that fits the current tree branch, to create the key sequence.
+	CmdKey levelKey;
+
+private:
+	// Enables the use of a vector for functions with different signatures,
+	// by bottlenecking to void *.
+	template<typename Signature, typename ...FuncArgs>
+	static void variableFunctionSignature(void *args, const type_info &info, Signature func);
 };
 
 #include "cmdNode.inc"
