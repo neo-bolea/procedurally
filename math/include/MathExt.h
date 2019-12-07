e4cbc0792bf57f7371ef7ea72dbb1e4271879882
Neo Bolea <neo@bolea.org>
2019-12-07 18:35:02 +0100

    Added Lerp and modified Clamp functions in MathExt.h.

--- math/include/MathExt.h
+++ math/include/MathExt.h
@@ -14,11 +14,17 @@ namespace Math
 //// Functions ////
 namespace Math
 {
-	template<typename T, REQUIRES(std::is_arithmetic_v<T>)> 
-	T Clamp(T n, T min, T max)
+	template<typename T, REQUIRES(std::is_arithmetic_v<T>)>
+	auto Clamp(T n, T min, T max) -> decltype(n)
 	{
 		n = (n < min) ? min : n;
 		n = (n > max) ? max : n;
 		return n;
+	}
+
+	template<typename T, REQUIRES(std::is_arithmetic_v<T>)>
+	auto Lerp(T start, T end, T t) -> decltype(t)
+	{
+		return start * (static_cast<T>(1) - t) + end * t;
 	}
 }
