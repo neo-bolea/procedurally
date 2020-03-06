bd08cd7431831a91f8ebac20092f5c90a04c2801
Neo Bolea <neo@bolea.org>
2020-03-06 20:19:48 +0100

    Changed CMakeLists.txt's to copy all public headers when building with CMake.

--- cmake/extras.cmake
+++ cmake/extras.cmake
@@ -1,9 +1,10 @@
 macro(GetCFiles LIB_NAME)
-    file(GLOB_RECURSE ${LIB_NAME}_SRCS *.cpp)
-    file(GLOB_RECURSE ${LIB_NAME}_PUB_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/include/*.h 
-                                              ${CMAKE_CURRENT_SOURCE_DIR}/include/*.inc)
-    file(GLOB_RECURSE ${LIB_NAME}_PRIV_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/*.h
-                                               ${CMAKE_CURRENT_SOURCE_DIR}/*.inc)
+    # Find all source files, public and private headers, and put them in separate variables.
+    file(GLOB_RECURSE ${LIB_NAME}_SRCS "*.cpp")
+    file(GLOB_RECURSE ${LIB_NAME}_PUB_HEADERS "${CMAKE_CURRENT_SOURCE_DIR}/include/*.h" 
+                                              "${CMAKE_CURRENT_SOURCE_DIR}/include/*.inc")
+    file(GLOB_RECURSE ${LIB_NAME}_PRIV_HEADERS "${CMAKE_CURRENT_SOURCE_DIR}/*.h"
+                                               "${CMAKE_CURRENT_SOURCE_DIR}/*.inc")
 endmacro(GetCFiles)
 
 macro(LibStandard LIB_NAME)
@@ -11,7 +12,9 @@ macro(LibStandard LIB_NAME)
 
     add_library(${LIB_NAME} ${${LIB_NAME}_SRCS} ${${LIB_NAME}_PRIV_HEADERS} ${${LIB_NAME}_PUB_HEADERS})
     target_include_directories(${LIB_NAME} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/include)
+
     target_link_libraries(${LIB_NAME} ${ARGN})
+    # Configure Visual Studio projects to use multiple cores when building.
     if(MSVC)
         target_compile_options(${LIB_NAME} PRIVATE "/MP")
     endif()
