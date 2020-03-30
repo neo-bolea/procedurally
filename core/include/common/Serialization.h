4ad46a210a1ae58b7f9a54ba7d3f6cb10a1511ff
neo-bolea <neo@bolea.org>
2020-03-30 20:24:53 +0200

    Added Serialization.h. Added (de-)serialization of std::vector.

---
+++ core/include/common/Serialization.h
@@ -0,0 +1,55 @@
+#include <ostream>
+#include <vector>
+
+template<typename Type, typename Alloc>
+std::ostream &operator<<(std::ostream &os, const std::vector<Type, Alloc> &v)
+{ 
+	os << '['; 
+	for(auto &it = v.begin(); it != v.end(); it++)
+	{ 
+		os << *it; 
+		if (it != v.end() - 1) { os << ','; }
+	} 
+	os << ']'; 
+
+	return os; 
+}
+
+template<typename Type, typename Alloc>
+std::istream &operator>>(std::istream &is, std::vector<Type, Alloc> &v)
+{ 
+	//do
+	//{
+	//	if (std::isdigit(is.peek()))
+	//	{
+	//		int i;
+	//		is >> i;
+	//		std::cout << i;
+	//	}
+	//	else
+	//	{
+	//		is.get();
+	//	}
+	//} while(is.peek() != ']');
+
+	std::stringstream ss;
+	ss << is.rdbuf();    
+
+	int tell;
+	if(ss.peek() == '[')
+	{
+		tell = ss.tellg();
+		ss.get();
+		tell = ss.tellg();
+		v.clear();
+		do
+		{
+			Type d;
+			ss >> d;
+			tell = ss.tellg();
+			v.push_back(d);
+		} while(ss.get() != ']');
+	}
+
+	return is; 
+}
