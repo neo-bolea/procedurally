b04682da5cd33e8b77a97395e13659ce2527501e
neo-bolea <neo@bolea.org>
2020-04-13 22:28:56 +0200

    Git had an error commiting changes when moving functions from GL.h/.cpp to Graphics.h/.cpp (as seen a few commits ago).

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -15,29 +15,27 @@ namespace Graphics
 	{
 		const GL::Program &program = GL::Programs::Tex2D();
 
-		static bool once = false;
-		static uint vao, vbo;
-		if(!once)
+		static GL::UniformInfo uModel, uColor, uUseTex, uDepth;
+		static GL::Mesh squareMesh;
+		if(!squareMesh.Initialized())
 		{
-			once = true;
-			glGenVertexArrays(1, &vao);
-			glGenBuffers(1, &vbo);
-		
-			glBindVertexArray(vao);
-			GLHelper::SetVBOData(vbo, GL::Models::Square2D::Verts, 2, 0);
-			GLHelper::SetVBOData(vbo, GL::Models::Square2D::UVs, 2, 1);
-			glBindVertexArray(0);
+			squareMesh.SetVertexAttribute(GL::Models::Square2D::Verts, 2, 0);
+			squareMesh.SetVertexAttribute(GL::Models::Square2D::UVs, 2, 1);
+
+			uModel = program.GetUniform("uModel");
+			uColor = program.GetUniform("uColor");
+			uUseTex = program.GetUniform("uUseTex");
+			uDepth = program.GetUniform("uDepth");
 		}
 		
 		GL::Program::Push(program);
 		
 		Math::Mat4 model;
-		//model = Math::GL::Scale(model, (fVec3)fVec2(1.f / size.x, 1.f / size.y));
-		model = Math::GL::Translate(model, (fVec3)(pos - fVec2(0.5f) * size));
+		model = Math::GL::Translate(model, (fVec3)pos);
 		model = Math::GL::Scale(model, (fVec3)size);
-		program.Set("uModel", model);
-		program.Set("uColor", color);
-		program.Set("uUseTex", (bool)texture);
+		program.Set(uModel, model);
+		program.Set(uColor, color);
+		program.Set(uUseTex, (bool)texture);
 #ifdef _DEBUG
 		if(depth <= -maxDepthRange || depth >= maxDepthRange)
 		{
@@ -47,10 +45,41 @@ namespace Graphics
 			depth = Math::Clamp(depth, -maxDepthRange+1, maxDepthRange-1);
 		}
 #endif
-		program.Set("uDepth", (depth + maxDepthRange) / (maxDepthRange * 2.f));
+		program.Set(uDepth, (depth + maxDepthRange) / (maxDepthRange * 2.f));
 
-		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, GL::Models::Square2D::Verts.size());
+		squareMesh.Use(GL::Models::Square2D::Verts.size());
 		GL::Program::Pop();
+	}
+
+
+	void DebugDrawDC(const std::vector<Vector2> &arr, float size, GLenum mode)
+	{
+		//Update size/width
+		switch(mode)
+		{
+		case GL::Points: glPointSize(size); break;
+		case GL::Lines: glLineWidth(size); break;
+		case GL::LineStrip: glLineWidth(size); break;
+		case GL::LineLoop: glLineWidth(size); break;
+		default:	UNDEFINED_CODE_PATH("The given OpenGL mode is not supported.");
+		}
+
+		glDisable(GL_DEPTH_TEST);
+
+		GL::Mesh mesh;
+		mesh.SetVertexAttribute(arr, 2, 0);
+		mesh.Use(arr.size());
+
+		glEnable(GL_DEPTH_TEST);
+	}
+
+	void DebugDrawScreen(const std::vector<Vector2> &screenArr, float size, GLenum mode)
+	{
+		std::vector<Vector2> dcArr(screenArr.size());
+		for(uint i = 0; i < screenArr.size(); i++)
+		{ 
+			//dcArr[i] = Screen::ScreenToDC(screenArr[i]);
+		}
+		DebugDrawDC(dcArr, size, mode);
 	}
 }
