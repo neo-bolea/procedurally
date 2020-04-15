f2de69bd68c1dc5dac5e6f8ba3df991a6deb118d
neo-bolea <neo@bolea.org>
2020-04-15 15:07:29 +0200

    Fixed bug in Mat.h/.inc where indices for access were in the reverse order (row-major instead of column-major).

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,13 +1,9 @@
 #include "Graphics/Camera.h"
 
 #include "Graphics/GL.h"
-#include "Math/MathExt.h"
 #include "Graphics/MathGL.h"
-#include "Common/GameTime.h"
 
 #include "GL/glew.h"
-
-#include <iostream>
 
 
 Camera2D::Camera2D(float screenRatio) : Ratio(screenRatio)
@@ -29,8 +25,8 @@ void Camera2D::Update()
 	proj = Math::GL::Orthographic(-VertSize * Ratio, VertSize * Ratio, -VertSize, VertSize, Near, Far);
 
 	glBindBuffer(GL_UNIFORM_BUFFER, ubo);
-	GLHelper::BindUBOData(0, 16 * sizeof(float), &view.v[0]);
-	GLHelper::BindUBOData(16 * sizeof(float), 16 * sizeof(float), &proj.v[0]);
-	GLHelper::BindUBOData(16 * sizeof(float) * 2, 16 * sizeof(float), &(proj * view).v[0]);
+	GLHelper::BindUBOData(0, 16 * sizeof(float), &view);
+	GLHelper::BindUBOData(16 * sizeof(float), 16 * sizeof(float), &proj);
+	GLHelper::BindUBOData(16 * sizeof(float) * 2, 16 * sizeof(float), &(proj * view));
 	glBindBuffer(GL_UNIFORM_BUFFER, 0);
 }
